0c4f1fd40e47c2fe89fabed73c040eab17bcf216
selaguardia <selaguardia91@gmail.com>
2022-03-30 10:22:04 -0700

    Title case a sentence (2nd way)

--- main.js
+++ main.js
@@ -96,12 +96,21 @@
 // console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
 
 //! Title case a sentence
-function titleCase(str) {
-  let words = str.toLowerCase().split(' ');
+// function titleCase(str) {
+//   let words = str.toLowerCase().split(' ');
   
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-  }
-  return words.join(' ');
+//   for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//   }
+//   return words.join(' ');
+// }
+// console.log(titleCase("I'm a little tea pot"))
+
+//! Title case a sentence (2nd way)
+function titleCase(str) {
+  let titled = str.toLowerCase().split(' ').map(function(elem) {
+    return elem[0].toUpperCase() + elem.slice(1);
+  })
+  return titled.join(' ')
 }
 console.log(titleCase("I'm a little tea pot"))
