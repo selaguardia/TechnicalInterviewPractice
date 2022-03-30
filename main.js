1ae49d8d554ffb6acf44e6df056ee640b6c24d88
selaguardia <selaguardia91@gmail.com>
2022-03-30 10:09:18 -0700

    Find the longest word in one line

--- main.js
+++ main.js
@@ -73,18 +73,24 @@
 
 
 //! Find the longest word
-function findLongestWordLength(str) {
-  let words = str.split(' ');
-  let longest = '';
-  console.log(words);
-  for (let word of words) {
-    console.log(word)
-    if (word.length > longest.length) {
-      longest = word;
-      console.log('longest===>',longest)
-    }
-  }
-  return longest.length;
-}
+// function findLongestWordLength(str) {
+//   let words = str.split(' ');
+//   let longest = '';
+//   console.log(words);
+//   for (let word of words) {
+//     console.log(word)
+//     if (word.length > longest.length) {
+//       longest = word;
+//       console.log('longest===>',longest)
+//     }
+//   }
+//   return longest.length;
+// }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//! Find the longest word in one line
+function findLongestWordLength(str) {
+  return str.split(' ').sort((a, b) => b.length - a.length)[0].length;
+}
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
