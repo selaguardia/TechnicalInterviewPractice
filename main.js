19f22f508d6ef9f8dae07c90f54f96138af7a786
selaguardia <selaguardia91@gmail.com>
2022-03-30 10:48:52 -0700

    confirm ending

--- main.js
+++ main.js
@@ -116,18 +116,28 @@
 // console.log(titleCase("I'm a little tea pot"))
 
 //! Return Largest Numbers in Arrays
-function largestOfFour(arr) {
-  let maxes = [];
-  for (let i = 0; i< arr.length; i++) {
-    let tempMax = arr[i][0];
-    for (var j = 0; j < arr[i].length; j++) {
-      let currentElement = arr[i][j];
-      if (currentElement >= tempMax) {
-        tempMax = currentElement;
-      }
-    }
-    maxes.push(tempMax)
+// function largestOfFour(arr) {
+//   let maxes = [];
+//   for (let i = 0; i< arr.length; i++) {
+//     let tempMax = arr[i][0];
+//     for (var j = 0; j < arr[i].length; j++) {
+//       let currentElement = arr[i][j];
+//       if (currentElement >= tempMax) {
+//         tempMax = currentElement;
+//       }
+//     }
+//     maxes.push(tempMax)
+//   }
+//   return maxes;
+// }
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+
+//! Confirm the Ending
+function confirmEnding(str, target) {
+  if (str.endsWith(target)) {
+    return true
   }
-  return maxes;
+  return false
 }
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+console.log(confirmEnding("Bastian", "n"));
+
