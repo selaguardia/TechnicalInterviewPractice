1734c20d07f5ff2274083e7b440f02f9cbe3150b
selaguardia <selaguardia91@gmail.com>
2022-05-02 15:01:18 -0700

    iterator reviews

--- main.js
+++ main.js
@@ -72,7 +72,18 @@ console.log(myArray2)
 //! Events
 // Real interactivity on a website requires event handlers. These are code structures that listen for activity in the browser, and run code in response. The most obvious example is handling the click event, which is fired by the browser when you click on something with your mouse.
 
+//! Iterators
+/*
+.forEach() is used to execute the same code on every element in an array but does not change the array and returns undefined.
 
+.map() executes the same code on every element in an array and returns a new array with the updated elements.
+
+.filter() checks every element in an array to see if it meets certain criteria and returns a new array with the elements that return truthy for the criteria.
+
+.findIndex() returns the index of the first element of an array that satisfies a condition in the callback function. It returns -1 if none of the elements in the array satisfies the condition.
+
+.reduce() iterates through an array and takes the values of the elements and returns a single value.
+*/
 
 
 
